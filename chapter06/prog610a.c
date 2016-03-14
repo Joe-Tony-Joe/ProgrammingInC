4a4b91e79622c2511e1432847e7490c199087e34
tendoasan <tjuywb@gmail.com>
2016-03-14 20:37:37 +0800

    生成质数表的程序(修订版)

--- chapter06/prog610a.c
+++ chapter06/prog610a.c
@@ -1,20 +1,22 @@
 // 生成质数表的程序(修订版) 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void){
 	int p, d;
-	_Bool isPrime;
+	bool isPrime;
 	
 	for(p = 2; p <= 50; ++p){
-		isPrime = 1;
+		isPrime = true;
 		
-		for(d = 2; d < p; ++d){
-			if(p % d == 0){
-				isPrime = 0;
-			}
-		if(isPrime != 0){
+		for(d = 2; d < p; ++d)
+			if(p % d == 0)
+				isPrime = false;
+			
+		if(isPrime != false)
 			printf("%i ", p);
-		}
-		
 	}
+	
+	printf("\n");
+	return 0;
 } 
