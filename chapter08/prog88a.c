fc6ad3a944a327c95ac531ca309dbcbbcb13d200
tendoasan <tjuywb@gmail.com>
2016-03-21 17:23:20 +0800

    找出数组中的最小值的函数

--- chapter08/prog88a.c
+++ chapter08/prog88a.c
@@ -7,10 +7,16 @@ float absoluteValue(float x){
 	return (x);
 } 
 
-// 计算某数平方根的函数
+// 计算某数平方根的函数，如果传入负数，将显示一条消息并返回-1 
 float squareRoot(float x){
 	const float epsilon = .00001;
 	float guess = 1.0;
+	float absoluteValue(float x);
+	
+	if(x < 0){
+		printf("Negative argument to squareRoot.\n");
+		return -1.0;
+	}
 	
 	while(absoluteValue(guess * guess - x) >= epsilon)
 		guess = (x / guess + guess) / 2.0;
