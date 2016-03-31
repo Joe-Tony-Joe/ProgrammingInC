44b81b8f46ff05ff69f5567a99dd9286317c6b9a
tendoasan <tjuywb@gmail.com>
2016-03-31 14:24:41 +0800

    将函数的形式参数声明为一个指针类型

---
+++ chapter11/prog1112.c
@@ -0,0 +1,22 @@
+// 将函数的形式参数声明为一个指针类型，作为一个指向数组的指针使用
+// 计算整数数组所有元素之和的函数
+
+#include <stdio.h>
+
+int arraySum(int *array, const int n){
+	int sum = 0;
+	int * const arrayEnd = array + n;
+	
+	for(; array < arrayEnd; ++array)
+		sum += *array;
+	
+	return sum;
+}
+
+int main(void){
+	int arraySum(int *array, const int n);
+	int values[10] = {3, 7, -9, 3, 6, -1, 7, 9, 1, -5};
+	
+	printf("The sum is %i\n", arraySum(values, 10));
+	return 0;
+}
